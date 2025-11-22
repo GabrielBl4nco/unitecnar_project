d078f94330aadb74ec2f651522ad809951ff111c
GabrielBl4nco <juangblancom@gmail.com>
2025-11-22 14:43:44 -0500

    Connect /pruebas/ to index template (render pruebas/index.html)

--- pruebas/views.py
+++ pruebas/views.py
@@ -1,21 +1,12 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def pruebas_view(request):
-        html = """<!doctype html>
-<html lang="es">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width,initial-scale=1">
-        <title>Pruebas</title>
-    </head>
-    <body>
-        <h1>Bienvenido a Django desde unitecnar</h1>
-    </body>
-</html>
-"""
-        return HttpResponse(html)
+    """Render the `pruebas/index.html` template so `/pruebas/` shows the same
+    page as the `template` route (index).
+    """
+    return render(request, 'pruebas/index.html')
 
 
 def template_view(request):
-        return render(request, 'pruebas/index.html')
+    return render(request, 'pruebas/index.html')
